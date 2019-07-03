7547466294d6df6fe60823b73eabe2393e534a38
Andres Felipe Mape <af.mape@uniandes.edu.co>
2019-07-03 15:48:45 -0500

    Tabla de ejemplo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TableBasicComponent } from './table-basic/table-basic.component';
+import { TableCompleteComponent } from './table-complete/table-complete.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "tablecomplete",
+    pathMatch: "full"
+  },
+  { path: 'tablebasic', component: TableBasicComponent },
+  { path: 'tablecomplete', component: TableCompleteComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableBasicComponent } from './table-basic/table-basic.component';
+import { NgbdSortableHeader } from './table-complete/sortable.directive';
+import { TableCompleteComponent } from './table-complete/table-complete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-
+    AppComponent,
+    TableBasicComponent,
+    TableCompleteComponent,
+    NgbdSortableHeader
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [NgbdSortableHeader],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
